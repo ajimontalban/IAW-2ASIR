3e41baae56011d537fc71a1222731efa4d8a8732
J. Ignacio M <jmonsar029@g.educaand.es>
2025-12-10 20:00:12 +0100

    Modificacion 2 R5E2

--- php/R5/R5E2.php
+++ php/R5/R5E2.php
@@ -1,13 +1,27 @@
 <?php
-function lanzamiento($n){
+function lanzamiento($n,&$serie){
+    $serie = array(
+        1 => 0, 
+        2 => 0,
+        3 => 0,
+        4 => 0,
+        5 => 0,
+        6 => 0);
     for ($i=0;$i<$n;$i++){
-        $valor[] = rand(1,6);
-
+        $valor = rand(1,6);
+        $serie[$valor]++;
     }
 }
+
 ?>
 <?php
 $n = $_GET['n'];
 
+lanzamiento($n,$serie);
+
+for ($i=1;$i<=6;$i++){
+    echo "<p>Número de veces que ha salido la cara $i: $serie[$i] "
+        . $porcentaje=(($serie[$i])/6) . "</p>";
+}
 
 ?>
