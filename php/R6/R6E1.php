a02d3e41a37e0db6316554a8ecf85afef6c2a563
J Ignacio M <jmonsar029@g.educaand.es>
2026-01-22 21:09:53 +0100

    Realizada prueba iaw

--- php/R6/R6E1.php
+++ php/R6/R6E1.php
@@ -4,9 +4,10 @@ function dni($a){
     $arr_aux = [];
     do{
     $dni = rand(10000000,99999999);
-    if (! isset($arr_aux[$dni]))
+    if (! isset($arr_aux[$dni])){
         $arr_aux[$dni] = 1;
         $alumnos[] = $dni;
+    }
     }while(count($arr_aux)<$a);
     return $alumnos;
 }
@@ -30,15 +31,6 @@ function matriz_num($a,$n){
     return $matriz;
 }
 
-//function matriz($a,$n){
-//    $alumnos = dni($a);
-//    $matriz = []; 
-//    for ($i=0;$i<count($alumnos);$i++){
-//        $matriz[$alumnos[$i]] = notas($n);
-//   }
-//   return $matriz;
-//}
-
 function nif($x){
     $letra = array("T","R","W","A","G","M","F","P","D","X","B","N","J","Z",
     "S","Q","V","H","L","C","K","E");
@@ -46,64 +38,78 @@ function nif($x){
     return $x;
 }
 
-function notamayor($i,$array){
-    // $i es el indice del examen que queremos acceder
-    $claves = array_keys($array);
-    if (!($array[$claves[0]][$i]))
-        exit("Ese examen no ha sido realizado!");
-
-    foreach($array as $clave => $valor){
-        reset($array[$clave]);
+function nota_mayor($array){
+    // asigno valor fuera del rango de notas
+    //$nota_may = -1;
+    $nota_may = $array[0][0];
+    for ($i=0;$i<count($array);$i++){
+        for ($j=0;$j< count($array[$i]);$j++){
+            if ($array[$i][$j] > $nota_may){
+                $nota_may = $array[$i][$j];
+            }else
+                continue;
+        } 
     }
-    $nota = -1;
-    foreach($array as $clave => $valor){
-        if ($nota < $array[$clave][$i])
-            $nota = $array[$clave][$i];
-        else
-            continue;
-    }
-    return $nota;
+    return $nota_may;
 }
 
-function notamenor($array,$y=0){
-    // $y es el indice del examen que queremos acceder
-
-    $claves = array_keys($array);
-    for ($i=0;$i<count($claves);$i++){
-        reset($array[$claves[$i]]);
+function nota_menor($array){
+    // asigno valor fuera del rango de notas
+    //$nota_men = 11;
+    $nota_men = [0][0];
+    for ($i=0;$i<count($array);$i++){
+        for ($j=0;$j< count($array[$i]);$j++){
+            if ($array[$i][$j] < $nota_men){
+                $nota_men = $array[$i][$j];
+            }else
+                continue;
+        } 
     }
-    if (!($array[$claves[0]][$y]))
-        exit("Ese examen no ha sido realizado!");
+    return $nota_men;
+}
 
-    $nota = 11;
-    foreach($array as $clave => $valor){
-        if ($nota > $array[$clave][$y])
-            $nota = $array[$clave][$y];
-        else
-            continue;
+function media($lista_notas){
+    $suma = 0;
+    for ($i=0;$i<count($lista_notas);$i++){
+        $suma += $lista_notas[$i]; 
     }
-    return $nota;
+    $media =$suma/count($lista_notas) ;
+    return $media;
+}
+
+function buscar_notas($array,$nota){
+    $indices = [];
+    for ($i=0;$i<count($array);$i++){
+        if (in_array($nota, $array[$i]))
+            $indices[] = $i;
+    }
+    return $indices;
+}
+
+function mostrar_alumnos($array_indice,$array_alumnos){
+    $vector = [];
+    for ($i=0;$i<count($array); $i++){
+       $vector[] = nif($array_alumnos[$array[$i]]);
+    }
+    return implode('-',$vector);
 }
 ?>
 <?php
 $a = $_GET['a'];
 $n = $_GET['n'];
-$i = $_GET['i'];
 
 $notas = matriz_num($a,$n);
-
 $alumnos = dni($a);
 for ($j=0;$j<count($notas);$j++){
-
-}
-foreach($notas as $alumno => $puntuaciones){
-    $persona = nif($alumno);
-    echo "<p> $persona => " . implode(', ', $puntuaciones) . "</p>";
+    $nif = nif($alumnos[$j]);
+    echo "<p> $nif => " . implode(', ',$notas[$j]) . " </p>";     
 }
 
-$notamax0 = notamayor($i,$notas);
-$notamin0 = notamenor($notas,$i);
+$notamax0 = nota_mayor($notas);
+$notamin0 = nota_menor($notas);
 
 echo "<p>La nota más alta es: $notamax0 </p>";
 echo "<p>La nota más baja es: $notamin0 </p>";
+
+$alumno_nota_baja = mostrar_alumnos();
 ?>
