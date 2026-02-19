74ec352f5a101624e956279015e148b94b433a59
J Ignacio M <jmonsar029@g.educaand.es>
2026-02-19 13:14:59 +0100

    Completado el formulario dinamico para el ejercicio 8-1

--- php/R8/R8E1.php
+++ php/R8/R8E1.php
@@ -1,3 +1,8 @@
+<?php
+$IP="10.30.0.10";
+$USER="root";
+$PASS="root";
+?>
 <html lang="es">
 <head>
     <title>Ejercicio 1 R8, conexiones con mysql</title>
@@ -5,18 +10,18 @@
 <body>
 <?php
 try{
-    $conexion = new mysqli("10.30.0.10","root","root");
+    $conexion = new mysqli($IP,$USER,$PASS);
 }
 catch(mysqli_sql_exception $error){
     echo "Error de conexión: " . $error->getMessage(). "<br/>";
     exit();
 }
 $conexion->select_db("bdPadron");
-$provincia = $_GET['n'];
+$provincia = $_GET['provincia'];
 $sql = "SELECT * FROM taMunicipios,taProvincias WHERE iRefProvincia=iCodProvincia AND vaNomProvincia like '$provincia'";
 $resultado = $conexion->query($sql);
 echo "<table border=\"1\">";
-echo "<tr><th>vaNomMunicipio</th></tr>";
+echo "<tr><th>Municipios</th></tr>";
 
 foreach($resultado as $fila){
     echo "<tr><td>" . $fila['vaNomMunicipio'] . "</td></tr>";
--- php/R9/R9E3.php
+++ php/R9/R9E3.php
@@ -13,11 +13,31 @@ $PASS="root";
 	<link href="css/style.css" rel="stylesheet" />
 </head>
 <body>
-    <form method="get" action="../php/R8E1.php">
+    <form method="get" action="R8E1.php">
         <label for="n">Provincia</label>
         <select name="provincia">
 <?php
+try{
 $conexion = new mysqli($IP,$USER,$PASS);
+}
+catch (mysqli_sql_exception $error){
+    echo "Error de conexión: " . $error->getMessage()."<br/>";
+}
+// Indicamos la bd a usar, se puede indicar tambien al hacer la primera conexion.
+$conexion->select_db("bdPadron");
+
+$sql = "SELECT vaNomProvincia FROM taProvincias";
+
+$resultado = $conexion->query($sql);
+
+$fila = $resultado->fetch_assoc();
+while ($fila){
+    echo "<option value=\"" . $fila['vaNomProvincia'] ."\">". strtoupper($fila['vaNomProvincia']). "</option>";
+    $fila = $resultado->fetch_assoc();
+}
+
+$resultado->close();
+$conexion->close();
 ?>
         </select>
         <button type="submit">Enviar</button>
